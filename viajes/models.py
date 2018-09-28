d16d27a8de691e3c38884ce136b6c94bb7b0a69e
r42 <lna.dev@outlook.com>
2018-09-28 03:35:59 -0300

    Pequeños cambios en el modelo Viaje Se modifica la longitud maxima de algunos campos

--- viajes/models.py
+++ viajes/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from datetime import datetime
 
 class Viaje(models.Model):
-    name = models.CharField(max_length=140)
-    destination = models.CharField(max_length=140)
-    career = models.CharField(max_length=140, blank=True, default="")
-    shortDescription = models.CharField(max_length=240)
+    name = models.CharField(max_length=80)
+    destination = models.CharField(max_length=80)
+    career = models.CharField(max_length=80, blank=True, default="")
+    shortDescription = models.CharField(max_length=140)
     longDescription = models.TextField()
     eventUrl = models.CharField(max_length=240, blank=True, default="")
     departureDate = models.DateField()
